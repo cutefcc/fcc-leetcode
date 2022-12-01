1542a94e84daac8d461fbf05e2040861e6a2797c
cutefcc <cutefcc@126.com>
2022-12-01 21:39:22 +0800

    feat: 合并两个已排序的数组，要求修改nums1，不需要返回值，时间复杂度为O(m + n)

---
+++ 88.merge-sorted-array.ts
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode id=88 lang=typescript
+ *
+ * [88] Merge Sorted Array
+ *
+ * https://leetcode.com/problems/merge-sorted-array/description/
+ *
+ * algorithms
+ * Easy (45.86%)
+ * Likes:    8460
+ * Dislikes: 755
+ * Total Accepted:    1.8M
+ * Total Submissions: 3.9M
+ * Testcase Example:  '[1,2,3,0,0,0]\n3\n[2,5,6]\n3'
+ *
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing
+ * order, and two integers m and n, representing the number of elements in
+ * nums1 and nums2 respectively.
+ *
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ *
+ * The final sorted array should not be returned by the function, but instead
+ * be stored inside the array nums1. To accommodate this, nums1 has a length of
+ * m + n, where the first m elements denote the elements that should be merged,
+ * and the last n elements are set to 0 and should be ignored. nums2 has a
+ * length of n.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * Output: [1,2,2,3,5,6]
+ * Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+ * The result of the merge is [1,2,2,3,5,6] with the underlined elements coming
+ * from nums1.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums1 = [1], m = 1, nums2 = [], n = 0
+ * Output: [1]
+ * Explanation: The arrays we are merging are [1] and [].
+ * The result of the merge is [1].
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+ * Output: [1]
+ * Explanation: The arrays we are merging are [] and [1].
+ * The result of the merge is [1].
+ * Note that because m = 0, there are no elements in nums1. The 0 is only there
+ * to ensure the merge result can fit in nums1.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * nums1.length == m + n
+ * nums2.length == n
+ * 0 <= m, n <= 200
+ * 1 <= m + n <= 200
+ * -10^9 <= nums1[i], nums2[j] <= 10^9
+ *
+ *
+ *
+ * Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+ *
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  // 合并两个已排序的数组，要求修改nums1，不需要返回值，时间复杂度为O(m + n)
+  let first: number = m - 1; // 数组nums1指针
+  let second: number = n - 1; // 数组nums2指针
+  let i: number = m + n - 1; // 计算结果的指针
+  while (second >= 0) {
+    let fVal = nums1[first];
+    let sVal = nums2[second];
+
+    if (fVal > sVal) {
+      nums1[i--] = fVal;
+      first--;
+    } else {
+      nums1[i--] = sVal;
+      second--;
+    }
+  }
+}
+// @lc code=end
