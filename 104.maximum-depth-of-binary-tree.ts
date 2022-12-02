47d00b01a6dccb98f8c6ed4d9a16c2c235b6065f
cutefcc <cutefcc@126.com>
2022-12-02 20:36:18 +0800

    feat: 计算二叉树的深度

---
+++ 104.maximum-depth-of-binary-tree.ts
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=104 lang=typescript
+ *
+ * [104] Maximum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (73.19%)
+ * Likes:    9099
+ * Dislikes: 149
+ * Total Accepted:    2.1M
+ * Total Submissions: 2.8M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given the root of a binary tree, return its maximum depth.
+ *
+ * A binary tree's maximum depth is the number of nodes along the longest path
+ * from the root node down to the farthest leaf node.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 3
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [1,null,2]
+ * Output: 2
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 10^4].
+ * -100 <= Node.val <= 100
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+  // 计算二叉树的深度，因为计算深度可以简化，一层一层往下简化，很适合递归
+  // 递归的退出条件
+  if (!root) {
+    return 0;
+  }
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+}
+// @lc code=end
