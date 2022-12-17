fc29537b66aaa60e10d0dc0563b2c5bc277f3f36
cutefcc <cutefcc@126.com>
2022-12-17 10:47:35 +0800

    feat: [7] Reverse Integer

---
+++ 7.reverse-integer.ts
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=7 lang=typescript
+ *
+ * [7] Reverse Integer
+ *
+ * https://leetcode.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Medium (27.26%)
+ * Likes:    9218
+ * Dislikes: 11229
+ * Total Accepted:    2.4M
+ * Total Submissions: 8.7M
+ * Testcase Example:  '123'
+ *
+ * Given a signed 32-bit integer x, return x with its digits reversed. If
+ * reversing x causes the value to go outside the signed 32-bit integer range
+ * [-2^31, 2^31 - 1], then return 0.
+ *
+ * Assume the environment does not allow you to store 64-bit integers (signed
+ * or unsigned).
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: x = 123
+ * Output: 321
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: x = -123
+ * Output: -321
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: x = 120
+ * Output: 21
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * -2^31 <= x <= 2^31 - 1
+ *
+ *
+ */
+
+// @lc code=start
+function reverse(x: number): number {
+  let res: number;
+  let stack: number[] = [];
+  if (x >= 0) {
+    while (x > 0) {
+      stack.push(x % 10);
+      x = Math.floor(x / 10);
+    }
+    while (stack[0] === 0) {
+      stack.splice(0, 1);
+    }
+    res = Number(stack.join(""));
+    if (res > Math.pow(2, 31)) res = 0;
+  } else {
+    x = -x;
+    res = -reverse(x);
+  }
+  return res;
+}
+// console.log(reverse(123));
+// @lc code=end
