cf1f5efbf66db69ed61375e7676abd12dd261ea2
cutefcc <cutefcc@126.com>
2022-12-24 11:47:44 +0800

    feat: 两两交换单链表中的节点-递归

---
+++ 24.swap-nodes-in-pairs.ts
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode id=24 lang=typescript
+ *
+ * [24] Swap Nodes in Pairs
+ *
+ * https://leetcode.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (60.54%)
+ * Likes:    8768
+ * Dislikes: 342
+ * Total Accepted:    1M
+ * Total Submissions: 1.7M
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * Given a linked list, swap every two adjacent nodes and return its head. You
+ * must solve the problem without modifying the values in the list's nodes
+ * (i.e., only nodes themselves may be changed.)
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,2,3,4]
+ * Output: [2,1,4,3]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = []
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: head = [1]
+ * Output: [1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the list is in the range [0, 100].
+ * 0 <= Node.val <= 100
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function swapPairs(head: ListNode | null): ListNode | null {
+  if (!head) return null;
+  let pre = head;
+  let curr = head.next;
+  if (!curr) {
+    return pre;
+  }
+
+  let next = curr.next; // 临时保存next
+  curr.next = pre; // 第二个元素指向第一个元素
+  pre.next = swapPairs(next); // 第一个元素指向后一对的 头节点
+  return curr; // 返回头节点
+}
+// @lc code=end
