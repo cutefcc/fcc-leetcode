68f4b831aef8c12399f6534aa2ecb085f45bc83b
cutefcc <cutefcc@126.com>
2023-01-19 15:38:45 +0800

    feat: 最长公共子序列的长度

---
+++ 1143.longest-common-subsequence.ts
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=1143 lang=typescript
+ *
+ * [1143] Longest Common Subsequence
+ *
+ * https://leetcode.com/problems/longest-common-subsequence/description/
+ *
+ * algorithms
+ * Medium (58.76%)
+ * Likes:    10002
+ * Dislikes: 118
+ * Total Accepted:    634.7K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '"abcde"\n"ace"'
+ *
+ * Given two strings text1 and text2, return the length of their longest common
+ * subsequence. If there is no common subsequence, return 0.
+ *
+ * A subsequence of a string is a new string generated from the original string
+ * with some characters (can be none) deleted without changing the relative
+ * order of the remaining characters.
+ *
+ *
+ * For example, "ace" is a subsequence of "abcde".
+ *
+ *
+ * A common subsequence of two strings is a subsequence that is common to both
+ * strings.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: text1 = "abcde", text2 = "ace"
+ * Output: 3
+ * Explanation: The longest common subsequence is "ace" and its length is 3.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: text1 = "abc", text2 = "abc"
+ * Output: 3
+ * Explanation: The longest common subsequence is "abc" and its length is 3.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: text1 = "abc", text2 = "def"
+ * Output: 0
+ * Explanation: There is no such common subsequence, so the result is 0.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= text1.length, text2.length <= 1000
+ * text1 and text2 consist of only lowercase English characters.
+ *
+ *
+ */
+
+// @lc code=start
+function longestCommonSubsequence(text1: string, text2: string): number {
+  // 求：最长公共子序列的长度
+  //一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+  // 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+  // 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+  let dp = Array.from(Array(text1.length + 1), () =>
+    Array(text2.length + 1).fill(0)
+  );
+
+  for (let i = 1; i <= text1.length; i++) {
+    for (let j = 1; j <= text2.length; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  return dp[text1.length][text2.length];
+}
+// @lc code=end
